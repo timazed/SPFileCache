69af52ff7f796ff9d9f4c8af1e3b7846683658af
Timothy Zelinsky <timazed@gmail.com>
2015-06-09 18:53:19 -0700

    Initial Commit

---
+++ SPFileCache/SPFileCache.h
@@ -0,0 +1,13 @@
+//
+//  SPFileCache.h
+//  SPFileCache
+//
+//  Created by Timothy Zelinsky on 9/06/2015.
+//  Copyright (c) 2015 zelinsky. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SPFileCache : NSObject
+
+@end
---
+++ SPFileCache/SPFileCache.m
@@ -0,0 +1,13 @@
+//
+//  SPFileCache.m
+//  SPFileCache
+//
+//  Created by Timothy Zelinsky on 9/06/2015.
+//  Copyright (c) 2015 zelinsky. All rights reserved.
+//
+
+#import "SPFileCache.h"
+
+@implementation SPFileCache
+
+@end
